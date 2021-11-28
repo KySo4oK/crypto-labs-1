9439cfe4e47c2ed4eee5e2c6e851bc8ffea19dca
KySo4oK <prostorostuk08@gmail.com>
2021-11-28 19:20:52 +0200

    refactor code

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,12 +7,10 @@ import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class App {
     static List<String> allSymbols = getAllSymbols();
     static List<String> alphabet = getResultSymbols();
-    static int countMax = 0;
     static List<String> results = new ArrayList<>();
 
     private static List<String> getResultSymbols() {
@@ -29,13 +27,12 @@ public class App {
     }
 
     public static void main(String[] args) throws DecoderException {
-//        task1();
+        task1();
         task2();
     }
 
     private static void task2() {
         String data = "G0IFOFVMLRAPI1QJbEQDbFEYOFEPJxAfI10JbEMFIUAAKRAfOVIfOFkYOUQFI15ML1kcJFUeYhA4IxAeKVQZL1VMOFgJbFMDIUAAKUgFOElMI1ZMOFgFPxADIlVMO1VMO1kAIBAZP1VMI14ANRAZPEAJPlMNP1VMIFUYOFUePxxMP19MOFgJbFsJNUMcLVMJbFkfbF8CIElMfgZNbGQDbFcJOBAYJFkfbF8CKRAeJVcEOBANOUQDIVEYJVMNIFwVbEkDORAbJVwAbEAeI1INLlwVbF4JKVRMOF9MOUMJbEMDIVVMP18eOBADKhALKV4JOFkPbFEAK18eJUQEIRBEO1gFL1hMO18eJ1UIbEQEKRAOKUMYbFwNP0RMNVUNPhlAbEMFIUUALUQJKBANIl4JLVwFIldMI0JMK0INKFkJIkRMKFUfL1UCOB5MH1UeJV8ZP1wVYBAbPlkYKRAFOBAeJVcEOBACI0dAbEkDORAbJVwAbF4JKVRMJURMOF9MKFUPJUAEKUJMOFgJbF4JNERMI14JbFEfbEcJIFxCbHIJLUJMJV5MIVkCKBxMOFgJPlVLPxACIxAfPFEPKUNCbDoEOEQcPwpDY1QDL0NCK18DK1wJYlMDIR8II1MZIVUCOB8IYwEkFQcoIB1ZJUQ1CAMvE1cHOVUuOkYuCkA4eHMJL3c8JWJffHIfDWIAGEA9Y1UIJURTOUMccUMELUIFIlc=";
-//        byDividingDataToBlocks(data);
         byIndex(data);
     }
 
@@ -63,55 +60,34 @@ public class App {
             }
             FrequencyTable frequencyTable = new FrequencyTable(flat.toString());
             String mostFrequented = frequencyTable.mostFrequented();
-            System.out.println(i);
-            if(i ==3) {
-                tryManual(flat.toString(), mostFrequented);
-            }
-//            maps.add(tryXor(mostFrequented, flat.toString()));
+            maps.add(tryXor(mostFrequented, flat.toString()));
         }
-//        joinMaps(maps);
-//        System.out.println(countMax);
-//        results.stream().distinct().forEach(r -> {
-//            System.out.println();
-//            System.out.println();
-//            System.out.println(r);
-//        });
-    }
-
-    private static void tryManual(String flat, String mostFrequented) {
-        alphabet.stream()
-                .map(s -> xor(mostFrequented, s))
-                .filter(key -> isNormalText(List.of(key), 0))
-                .forEach(key -> {
-                    String collect = getSymbols(flat)
-                            .stream()
-                            .map(x -> xor(key, x))
-                            .collect(Collectors.joining());
-//                    if (isNormalText(getSymbols(collect), 150)) {
-                        System.out.println(collect);
-                        System.out.println("---" + key + "---" + xor(mostFrequented, key));
-//                    }
-                });
+        joinMaps(maps);
+        results.stream().distinct().forEach(r -> {
+            System.out.println();
+            System.out.println();
+            System.out.println(r);
+        });
     }
 
     private static void joinMaps(List<Map<String, String>> maps) {
         List<Map.Entry<String, String>> first = maps.get(0).entrySet().stream().toList();
-//        List<Map.Entry<String, String>> second = maps.get(1).entrySet().stream().toList();
+        List<Map.Entry<String, String>> second = maps.get(1).entrySet().stream().toList();
         List<Map.Entry<String, String>> third = maps.get(2).entrySet().stream().toList();
         List<Map.Entry<String, String>> fourth = maps.get(3).entrySet().stream().toList();
         for (int i = 0; i < first.size(); i++) {
-//            for (int j = 0; j < second.size(); j++) {
+            for (int j = 0; j < second.size(); j++) {
                 for (int k = 0; k < third.size(); k++) {
                     for (int x = 0; x < fourth.size(); x++) {
                         Map.Entry<String, String> e1 = first.get(i);
-//                        Map.Entry<String, String> e2 = second.get(i);
+                        Map.Entry<String, String> e2 = second.get(i);
                         Map.Entry<String, String> e3 = third.get(i);
                         Map.Entry<String, String> e4 = first.get(i);
                         String result = e1.getKey()
-//                                + e2.getKey()
+                                + e2.getKey()
                                 + e3.getKey() + e4.getKey();
                         result += "\n" + e1;
-//                        result += "\n" + e2;
+                        result += "\n" + e2;
                         result += "\n" + e3;
                         result += "\n" + e4;
                         if(isNormalText(getSymbols(result), 450)) {
@@ -120,7 +96,7 @@ public class App {
                     }
                 }
             }
-//        }
+        }
 
     }
 
@@ -142,9 +118,6 @@ public class App {
 
     private static boolean isNormalText(List<String> symbols, int i) {
         long count = symbols.stream().filter(symbol -> alphabet.contains(symbol)).count();
-        if(count > countMax) {
-            countMax = (int) count;
-        }
         return count > i;
     }
 
@@ -167,84 +140,14 @@ public class App {
         return result;
     }
 
-    private static void byDividingDataToBlocks(String data) {
-        double max = 0.0;
-        double heightMax = 1;
-        for (int height = 1; height < 8; height++) {
-            double frequency = maxFreqByHeight(data, height);
-            if (frequency > max) {
-                max = frequency;
-                heightMax = height;
-            }
-        }
-        System.out.println(max + " | " + heightMax);
-
-        System.out.println(maxFreqByHeight1(data, ((int) heightMax)));
-    }
-
-    private static double maxFreqByHeight(String data, int height) {
-        List<String> dataList = getSymbols(data);
-        double max = 0.0;
-        for (int stage = 0; stage < height; stage++) {
-            StringBuilder result = new StringBuilder();
-            for (int i = 0; i <= dataList.size() / height + 1; i++) {
-                int index = i * height + stage;
-                if (dataList.size() > index) {
-                    result.append(dataList.get(index));
-                }
-            }
-            FrequencyTable table = new FrequencyTable(result.toString());
-            double v = table.biggestFrequency();
-            if (v > max) {
-                max = v;
-            }
-        }
-        return max;
-    }
-
-    private static String maxFreqByHeight1(String data, int height) {
-        List<String> dataList = getSymbols(data);
-        List<String> resultLines = new ArrayList<>();
-        for (int stage = 0; stage < height; stage++) {
-            StringBuilder result = new StringBuilder();
-            for (int i = 0; i <= dataList.size() / height + 1; i++) {
-                int index = i * height + stage;
-                if (dataList.size() > index) {
-                    result.append(dataList.get(index));
-                }
-            }
-            String text = result.toString();
-            FrequencyTable table = new FrequencyTable(text);
-            String mostFrequented = table.mostFrequented();
-            String key = xor(mostFrequented, " ");
-            System.out.println(key);
-            resultLines.add(getSymbols(text)
-                    .stream()
-                    .map(s -> xor(key, s))
-                    .collect(Collectors.joining()));
-        }
-        StringBuilder result = new StringBuilder();
-
-        for (int h = 0; h < resultLines.get(0).length(); h++) {
-            for (String resultLine : resultLines) {
-                if(resultLine.length() > h) {
-                    result.append(resultLine.charAt(h));
-                }
-            }
-        }
-
-        return result.toString();
-    }
-
     private static void task1() throws DecoderException {
         String data = "7958401743454e1756174552475256435e59501a5c524e176f786517545e475f5245191772195019175e4317445f58425b531743565c521756174443455e595017d5b7ab5f525b5b58174058455b53d5b7aa175659531b17505e41525917435f52175c524e175e4417d5b7ab5c524ed5b7aa1b174f584517435f5217515e454443175b524343524517d5b7ab5fd5b7aa17405e435f17d5b7ab5cd5b7aa1b17435f5259174f584517d5b7ab52d5b7aa17405e435f17d5b7ab52d5b7aa1b17435f525917d5b7ab5bd5b7aa17405e435f17d5b7ab4ed5b7aa1b1756595317435f5259174f58451759524f4317545f564517d5b7ab5bd5b7aa17405e435f17d5b7ab5cd5b7aa175650565e591b17435f525917d5b7ab58d5b7aa17405e435f17d5b7ab52d5b7aa1756595317445817585919176e5842175a564e17424452175659175e5953524f1758511754585e59545e53525954521b177f565a5a5e595017535e4443565954521b177c56445e445c5e17524f565a5e5956435e58591b17444356435e44435e54565b17435244434417584517405f564352415245175a52435f5853174e5842175152525b174058425b5317445f584017435f52175552444317455244425b4319";
 
         String text = fromHex(data);
 
         String x = new FrequencyTable(text).mostFrequented();
-        System.out.println(x);
 
-        tryw1(text, x);
+        tryWithSpaceAsMostFrequented(text, x);
     }
 
     static List<String> getSymbols(String text) {
@@ -261,9 +164,8 @@ public class App {
         return x;
     }
 
-    private static void tryw1(String d, String x) {
+    private static void tryWithSpaceAsMostFrequented(String d, String x) {
         System.out.println(d);
-//        String key = xor(String.valueOf(d.charAt(0)), String.valueOf(d.charAt(1)));
         String key = xor(" ", x);
         System.out.println(key);
         List<String> elements = getSymbols(d);
